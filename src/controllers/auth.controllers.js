a29e03f7a6e46d96fdc347bdfb729a4a22a5152f
Ravent Dahiya <raventdahiya2003@gmail.com>
2025-07-15 12:58:27 +0530

    added sameSite: "strict" attribute  to cookies

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -104,6 +104,7 @@ const loginUser = asyncHandler(async (req, res) => {
   const cookieOptions = {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
     maxAge: 24 * 60 * 60 * 1000,
   };
 
